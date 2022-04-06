6259b69decbc63c53650caf63d8dab4741c0e206
FancyBananasbsc <103136913+FancyBananasbsc@users.noreply.github.com>
2022-04-06 19:03:18 +0200

    Add files via upload

---
+++ distributor_flat.sol
@@ -0,0 +1,496 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.7.4;
+
+library SafeMathInt {
+    int256 private constant MIN_INT256 = int256(1) << 255;
+    int256 private constant MAX_INT256 = ~(int256(1) << 255);
+
+    function mul(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a * b;
+
+        require(c != MIN_INT256 || (a & MIN_INT256) != (b & MIN_INT256));
+        require((b == 0) || (c / b == a));
+        return c;
+    }
+
+    function div(int256 a, int256 b) internal pure returns (int256) {
+        require(b != -1 || a != MIN_INT256);
+
+        return a / b;
+    }
+
+    function sub(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a - b;
+        require((b >= 0 && c <= a) || (b < 0 && c > a));
+        return c;
+    }
+
+    function add(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a + b;
+        require((b >= 0 && c >= a) || (b < 0 && c < a));
+        return c;
+    }
+
+    function abs(int256 a) internal pure returns (int256) {
+        require(a != MIN_INT256);
+        return a < 0 ? -a : a;
+    }
+}
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+
+interface IPancakeSwapPair {
+		event Approval(address indexed owner, address indexed spender, uint value);
+		event Transfer(address indexed from, address indexed to, uint value);
+
+		function name() external pure returns (string memory);
+		function symbol() external pure returns (string memory);
+		function decimals() external pure returns (uint8);
+		function totalSupply() external view returns (uint);
+		function balanceOf(address owner) external view returns (uint);
+		function allowance(address owner, address spender) external view returns (uint);
+
+		function approve(address spender, uint value) external returns (bool);
+		function transfer(address to, uint value) external returns (bool);
+		function transferFrom(address from, address to, uint value) external returns (bool);
+
+		function DOMAIN_SEPARATOR() external view returns (bytes32);
+		function PERMIT_TYPEHASH() external pure returns (bytes32);
+		function nonces(address owner) external view returns (uint);
+
+		function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+		event Mint(address indexed sender, uint amount0, uint amount1);
+		event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+		event Swap(
+				address indexed sender,
+				uint amount0In,
+				uint amount1In,
+				uint amount0Out,
+				uint amount1Out,
+				address indexed to
+		);
+		event Sync(uint112 reserve0, uint112 reserve1);
+
+		function MINIMUM_LIQUIDITY() external pure returns (uint);
+		function factory() external view returns (address);
+		function token0() external view returns (address);
+		function token1() external view returns (address);
+		function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+		function price0CumulativeLast() external view returns (uint);
+		function price1CumulativeLast() external view returns (uint);
+		function kLast() external view returns (uint);
+
+		function mint(address to) external returns (uint liquidity);
+		function burn(address to) external returns (uint amount0, uint amount1);
+		function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+		function skim(address to) external;
+		function sync() external;
+
+		function initialize(address, address) external;
+}
+
+interface IPancakeSwapRouter{
+		function factory() external pure returns (address);
+		function WETH() external pure returns (address);
+
+		function addLiquidity(
+				address tokenA,
+				address tokenB,
+				uint amountADesired,
+				uint amountBDesired,
+				uint amountAMin,
+				uint amountBMin,
+				address to,
+				uint deadline
+		) external returns (uint amountA, uint amountB, uint liquidity);
+		function addLiquidityETH(
+				address token,
+				uint amountTokenDesired,
+				uint amountTokenMin,
+				uint amountETHMin,
+				address to,
+				uint deadline
+		) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+		function removeLiquidity(
+				address tokenA,
+				address tokenB,
+				uint liquidity,
+				uint amountAMin,
+				uint amountBMin,
+				address to,
+				uint deadline
+		) external returns (uint amountA, uint amountB);
+		function removeLiquidityETH(
+				address token,
+				uint liquidity,
+				uint amountTokenMin,
+				uint amountETHMin,
+				address to,
+				uint deadline
+		) external returns (uint amountToken, uint amountETH);
+		function removeLiquidityWithPermit(
+				address tokenA,
+				address tokenB,
+				uint liquidity,
+				uint amountAMin,
+				uint amountBMin,
+				address to,
+				uint deadline,
+				bool approveMax, uint8 v, bytes32 r, bytes32 s
+		) external returns (uint amountA, uint amountB);
+		function removeLiquidityETHWithPermit(
+				address token,
+				uint liquidity,
+				uint amountTokenMin,
+				uint amountETHMin,
+				address to,
+				uint deadline,
+				bool approveMax, uint8 v, bytes32 r, bytes32 s
+		) external returns (uint amountToken, uint amountETH);
+		function swapExactTokensForTokens(
+				uint amountIn,
+				uint amountOutMin,
+				address[] calldata path,
+				address to,
+				uint deadline
+		) external returns (uint[] memory amounts);
+		function swapTokensForExactTokens(
+				uint amountOut,
+				uint amountInMax,
+				address[] calldata path,
+				address to,
+				uint deadline
+		) external returns (uint[] memory amounts);
+		function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+				external
+				payable
+				returns (uint[] memory amounts);
+		function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+				external
+				returns (uint[] memory amounts);
+		function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+				external
+				returns (uint[] memory amounts);
+		function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+				external
+				payable
+				returns (uint[] memory amounts);
+
+		function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+		function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+		function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+		function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+		function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+		function removeLiquidityETHSupportingFeeOnTransferTokens(
+			address token,
+			uint liquidity,
+			uint amountTokenMin,
+			uint amountETHMin,
+			address to,
+			uint deadline
+		) external returns (uint amountETH);
+		function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+			address token,
+			uint liquidity,
+			uint amountTokenMin,
+			uint amountETHMin,
+			address to,
+			uint deadline,
+			bool approveMax, uint8 v, bytes32 r, bytes32 s
+		) external returns (uint amountETH);
+	
+		function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+			uint amountIn,
+			uint amountOutMin,
+			address[] calldata path,
+			address to,
+			uint deadline
+		) external;
+		function swapExactETHForTokensSupportingFeeOnTransferTokens(
+			uint amountOutMin,
+			address[] calldata path,
+			address to,
+			uint deadline
+		) external payable;
+		function swapExactTokensForETHSupportingFeeOnTransferTokens(
+			uint amountIn,
+			uint amountOutMin,
+			address[] calldata path,
+			address to,
+			uint deadline
+		) external;
+}
+
+interface IPancakeSwapFactory {
+		event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+		function feeTo() external view returns (address);
+		function feeToSetter() external view returns (address);
+
+		function getPair(address tokenA, address tokenB) external view returns (address pair);
+		function allPairs(uint) external view returns (address pair);
+		function allPairsLength() external view returns (uint);
+
+		function createPair(address tokenA, address tokenB) external returns (address pair);
+
+		function setFeeTo(address) external;
+		function setFeeToSetter(address) external;
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+interface IDividendDistributor {
+    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external;
+    function setShare(address shareholder, uint256 amount) external;
+    function deposit() external payable;
+    function process(uint256 gas) external;
+}
+
+contract DividendDistributor is IDividendDistributor {
+    using SafeMath for uint256;
+
+    mapping (address => bool) _allowed;
+
+    struct Share {
+        uint256 amount;
+        uint256 totalExcluded;
+        uint256 totalRealised;
+    }
+
+    IERC20 BUSD = IERC20(0x0078867bbeef44f2326bf8ddd1941a4439382ef2a7); //TESTNET
+    //IERC20 BUSD = IERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56); // MAINNET
+    IPancakeSwapRouter router;
+
+    address[] shareholders;
+    mapping (address => uint256) shareholderIndexes;
+    mapping (address => uint256) shareholderClaims;
+
+    mapping (address => Share) public shares;
+
+    uint256 public totalShares;
+    uint256 public totalDividends;
+    uint256 public totalDistributed;
+    uint256 public dividendsPerShare;
+    uint256 public currentIndex;
+
+    uint256 public dividendsPerShareAccuracyFactor = 10 ** 36;
+    uint256 public minPeriod = 1 hours;
+    uint256 public minDistribution = 1 * (10 ** 18);
+
+    bool initialized;
+    modifier initialization() {
+        require(!initialized);
+        _;
+        initialized = true;
+    }
+
+    modifier onlyAllowed() {
+        require(_allowed[msg.sender]); _;
+    }
+
+    constructor () {
+        //router =  IPancakeSwapRouter(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3); //TESTNET
+        router =  IPancakeSwapRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E); //MAINNET
+        _allowed[msg.sender] = true;
+    }
+
+    function setAllowed(address _address, bool _allow) external onlyAllowed {
+        require(_address != msg.sender);
+        _allowed[_address] = _allow;
+    }
+
+    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external override onlyAllowed {
+        minPeriod = _minPeriod;
+        minDistribution = _minDistribution;
+    }
+
+    function setShare(address shareholder, uint256 amount) external override onlyAllowed {
+        if(shares[shareholder].amount > 0){
+            distributeDividend(shareholder);
+        }
+
+        if(amount > 0 && shares[shareholder].amount == 0){
+            addShareholder(shareholder);
+        }else if(amount == 0 && shares[shareholder].amount > 0){
+            removeShareholder(shareholder);
+        }
+
+        totalShares = totalShares.sub(shares[shareholder].amount).add(amount);
+        shares[shareholder].amount = amount;
+        shares[shareholder].totalExcluded = getCumulativeDividends(shares[shareholder].amount);
+    }
+
+    function deposit() external payable override onlyAllowed {
+        uint256 balanceBefore = BUSD.balanceOf(address(this));
+
+        address[] memory path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = address(BUSD);
+
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        uint256 amount = BUSD.balanceOf(address(this)).sub(balanceBefore);
+
+        totalDividends = totalDividends.add(amount);
+        dividendsPerShare = dividendsPerShare.add(dividendsPerShareAccuracyFactor.mul(amount).div(totalShares));
+    }
+
+    function process(uint256 gas) external override onlyAllowed {
+        uint256 shareholderCount = shareholders.length;
+
+        if(shareholderCount == 0) { return; }
+
+        uint256 gasUsed = 0;
+        uint256 gasLeft = gasleft();
+        uint256 iterations = 0;
+
+        while(gasUsed < gas && iterations < shareholderCount) {
+            if(currentIndex >= shareholderCount){
+                currentIndex = 0;
+            }
+
+            if(shouldDistribute(shareholders[currentIndex])){
+                distributeDividend(shareholders[currentIndex]);
+            }
+
+            gasUsed = gasUsed.add(gasLeft.sub(gasleft()));
+            gasLeft = gasleft();
+            currentIndex++;
+            iterations++;
+        }
+    }
+
+    function shouldDistribute(address shareholder) internal view returns (bool) {
+        return shareholderClaims[shareholder] + minPeriod < block.timestamp && getUnpaidEarnings(shareholder) > minDistribution;
+    }
+
+    function distributeDividend(address shareholder) internal {
+        if(shares[shareholder].amount == 0){ return; }
+
+        uint256 amount = getUnpaidEarnings(shareholder);
+        if(amount > 0){
+            totalDistributed = totalDistributed.add(amount);
+            BUSD.transfer(shareholder, amount);
+            shareholderClaims[shareholder] = block.timestamp;
+            shares[shareholder].totalRealised = shares[shareholder].totalRealised.add(amount);
+            shares[shareholder].totalExcluded = getCumulativeDividends(shares[shareholder].amount);
+        }
+    }
+
+    function claimDividend() external {
+        distributeDividend(msg.sender);
+    }
+
+    function getUnpaidEarnings(address shareholder) public view returns (uint256) {
+        if(shares[shareholder].amount == 0){ return 0; }
+
+        uint256 shareholderTotalDividends = getCumulativeDividends(shares[shareholder].amount);
+        uint256 shareholderTotalExcluded = shares[shareholder].totalExcluded;
+
+        if(shareholderTotalDividends <= shareholderTotalExcluded){ return 0; }
+
+        return shareholderTotalDividends.sub(shareholderTotalExcluded);
+    }
+
+    function getCumulativeDividends(uint256 share) internal view returns (uint256) {
+        return share.mul(dividendsPerShare).div(dividendsPerShareAccuracyFactor);
+    }
+
+    function addShareholder(address shareholder) internal {
+        shareholderIndexes[shareholder] = shareholders.length;
+        shareholders.push(shareholder);
+    }
+
+    function removeShareholder(address shareholder) internal {
+        shareholders[shareholderIndexes[shareholder]] = shareholders[shareholders.length-1];
+        shareholderIndexes[shareholders[shareholders.length-1]] = shareholderIndexes[shareholder];
+        shareholders.pop();
+    }
+}
+
